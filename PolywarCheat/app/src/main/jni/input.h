4cf4959a7e4546f23aa14f8ee4df406161daa4ae
AizGremory <yosirianto999@gmail.com>
2026-07-11 18:57:55 +0700

    Delete PolywarCheat directory

--- PolywarCheat/app/src/main/jni/input.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <pthread.h>
-
-// ImGui is updated from the EGL render thread. Keep a mutex available for
-// future producers, but do not hook Android's private libinput C++ ABI: its
-// signatures vary between Android/vendor builds and can crash on the first
-// MotionEvent. Touch state is polled from UnityEngine.Input in Main.cpp.
-pthread_mutex_t g_ImGuiMutex = PTHREAD_MUTEX_INITIALIZER;
